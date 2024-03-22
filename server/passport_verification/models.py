fba2b81da0564fa8a21568ccb93ed57bdf9e9754
Bogdans <bigdan110702@gmail.com>
2024-03-22 14:11:27 +0200

    adding classes to auto_manager

--- server/passport_verification/models.py
+++ server/passport_verification/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Driver(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=72)
-    documentNumber = models.CharField(max_length=15)
+    document_number = models.CharField(max_length=15)
     date = models.DateTimeField()
