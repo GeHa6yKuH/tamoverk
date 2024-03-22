e4bf0f88fd690a7efd42cfaf7c27ee74e0016a75
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-03-22 12:23:18 +0100

    created services

---
+++ server/database_access_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DatabaseAccessServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'database_access_service'
---
+++ server/passport_verification_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PassportVerificationServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'passport_verification_service'
