7e2aa3a7f4c1816b231ea52b806e8119c521eb84
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-03-22 12:40:50 +0100

    created model for auth service

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class EmployeePosition(models.TextChoices):
+    INSPECTOR = 'INSPECTOR'
+    SUPERVISOR = 'SUPERVISOR'
+    DOCUMENT_VERIFIER = 'DOCUMENTVERIFIER'
+
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=300)
+    phoneNumber = models.CharField(max_length=15)
+    name = models.CharField(max_length=72)
+    password = models.CharField(max_length=56)
+    location = models.CharField(max_length=250)
+    position = models.Choices(EmployeePosition)
