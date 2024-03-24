70b4d62311fbcff62ad556a276322744a251a9b3
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-03-24 19:57:46 +0100

    update project

--- server/license_plate_recognition/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LicensePlateServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'license_plate_recognition'
--- server/passport_verification/models.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from django.db import models
-
-
-class Driver(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=72)
-    document_number = models.CharField(max_length=15)
-    date = models.DateTimeField()
