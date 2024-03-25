18e3c3d7a282527c11c3c72a908135e8f2f851a7
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-03-25 01:11:01 +0100

    add model for auth service

--- auth_service/apps.py
+++ auth_service/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationConfig(AppConfig):
+class AuthServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name = 'auth_service'
